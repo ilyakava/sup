5ff409f026754233815e5545cb71c65cf5caec35
Ilya Kavalerov <ilya@artsymail.com>
2014-03-21 16:04:37 -0400

    pre deploy changes

---
+++ config/initializers/setup_email.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => ENV['SMTP_ADDRESS'] || "smtp.gmail.com",
+  :port                 => ENV['SMTP_PORT'] || 587,
+  :domain               => ENV['SMTP_DOMAIN'] || "xtal.artsy.net",
+  :user_name            => ENV['SMTP_USER_NAME'],
+  :password             => ENV['SMTP_PASSWORD'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
