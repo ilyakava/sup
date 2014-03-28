14477ce9246fc95f328424bd38f3538274132f04
Ilya Kavalerov <ilya@artsymail.com>
2014-03-28 17:59:54 -0400

    Replace a time object with a forgotten array

--- app/mailers/meeting_mailer.rb
+++ app/mailers/meeting_mailer.rb
@@ -3,13 +3,13 @@ class MeetingMailer < ActionMailer::Base
 
   def new_meeting(meeting)
     @members = meeting.members
-    @times = meeting.meeting_date
+    @times = [meeting.meeting_date]
     mail(to: @members.map(&:email), subject: "S'up with #{@members[0]}, #{@members[1]}, and #{@members[2]}?")
   end
 
   def new_meeting_debug(meeting)
     @members = meeting.members
-    @times = meeting.meeting_date
+    @times = [meeting.meeting_date]
     mail(to: "ilya@artsymail.com",
       subject: "[SUP DEBUG] S'up with #{@members[0]}, #{@members[1]}, and #{@members[2]}?",
       template_name: 'new_meeting')
