dbffb1f0f131811cfcc379967cb76df702a83363
Ilya Kavalerov <ilya@artsymail.com>
2014-05-01 16:20:34 -0400

    WIP monte carlo optimization with depth first search for valid triplet groups of target size, about to test

--- app/helpers/cost.rb
+++ app/helpers/cost.rb
@@ -4,14 +4,12 @@ module Cost
   class Helper
 
     SHARED_GROUP = Float::INFINITY
-    def self.all_triplets
-      Member.all.pluck(:id).shuffle.combination(3).to_a
-    end
 
     def initialize
-      all_triplets_flat = Helper.all_triplets.flatten
+      @all_triplets = Member.all.pluck(:id).shuffle.combination(3).to_a
+      all_triplets_flat = @all_triplets.flatten
       # should be a hash where for every key (a member id) the value is an array
-      # of indexes in Helper.all_triplets where the member id does not occur
+      # of indexes in @all_triplets where the member id does not occur
       # Then take the intersection of these values
       @member_id_to_non_membered_triplet_indexes = Hash[
         Member.all.map do |member|
@@ -39,36 +37,43 @@ module Cost
     end
 
     def enumerator(target_num_triplets)
-      # starting the set should be arbitrary
-      set_of_arr_of_trips = Set.new([[Helper.all_triplets.first]])
-      until set_of_arr_of_trips.first.length == target_num_triplets
-        set_of_arr_of_trips = add_one_more_trip_to_set(set_of_arr_of_trips)
+      Enumerator.new do |output|
+        batch(target_num_triplets, output)
       end
+    end
 
-      Enumerator.new do |o|
-        set_of_arr_of_trips.each do |arr_of_trips|
-          o << arr_of_trips
+    def batch(target_num_triplets, yielder, arr_of_trips = [])
+      if arr_of_trips.length == target_num_triplets
+        yielder << arr_of_trips
+      else
+        valid_addition_indexes = if arr_of_trips.empty?
+          (0...(@all_triplets.length)).to_a
+        else
+          arr_of_trips.flatten.map { |id| h[id] }.inject(&:&)
+        end
+        valid_addition_indexes.each do |addition_index|
+          batch(target_num_triplets, [*arr_of_trips, @all_triplets[addition_index]])
         end
       end
     end
 
 
 
-    def add_one_more_trip_to_set(set_of_arr_of_trips)
-      puts "add_one_more_trip_to_set with #{set_of_arr_of_trips.first.length}"
-      new_set = Set.new
-      h = @member_id_to_non_membered_triplet_indexes
-      set_of_arr_of_trips.each do |arr_of_trips|
-        member_ids_so_far = arr_of_trips.flatten
-        # get the allowed indexes for each member_id, and take the intersection
-        allowed_triplet_indexes = member_ids_so_far.map { |id| h[id] }.inject(&:&)
-        # binding.pry if allowed_triplet_indexes == false
-        puts "iterating on #{allowed_triplet_indexes.length}"
-        allowed_triplet_indexes.each do |idx|
-          new_set << [Helper.all_triplets[idx], *arr_of_trips]
-        end
-      end
-      new_set
-    end
+    # def add_one_more_trip_to_set(set_of_arr_of_trips)
+    #   puts "add_one_more_trip_to_set with #{set_of_arr_of_trips.first.length}"
+    #   new_set = Set.new
+    #   h = @member_id_to_non_membered_triplet_indexes
+    #   set_of_arr_of_trips.each do |arr_of_trips|
+    #     member_ids_so_far = arr_of_trips.flatten
+    #     # get the allowed indexes for each member_id, and take the intersection
+    #     allowed_triplet_indexes = member_ids_so_far.map { |id| h[id] }.inject(&:&)
+    #     # binding.pry if allowed_triplet_indexes == false
+    #     puts "iterating on #{allowed_triplet_indexes.length}"
+    #     allowed_triplet_indexes.each do |idx|
+    #       new_set << [@all_triplets[idx], *arr_of_trips]
+    #     end
+    #   end
+    #   new_set
+    # end
   end
 end
