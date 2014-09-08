5eb690e11833fee580d85389bf7e12cd2908b808
Ilya Kavalerov <ilya@artsymail.com>
2014-09-08 11:40:28 -0400

    asset compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,7 @@ Sup::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb', '*.scss.erb', '*.scss']
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
